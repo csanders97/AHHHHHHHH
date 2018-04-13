0cc38915e7a9b00f387b07cf82dfaad523646611
Caleb Sanders <colonelsanders217@gmail.com>
2018-04-12 22:37:51 -0600

    ComponentsFactory created

---
+++ ComponentsFactory.py
@@ -0,0 +1,57 @@
+class BaseComponent(object):
+
+    def __init__(self):
+        self.width = 1
+        self.height = 1
+
+    def get_width(self):
+        return self.width
+
+    def get_height(self):
+        return self.height
+
+
+class ButtonComponent(BaseComponent):
+
+    def __init__(self):
+        self.width = 10
+        self.height = 10
+
+
+class CanvasComponent(BaseComponent):
+
+    def __init__(self):
+        self.width = 20
+        self.height = 20
+
+
+class TextBoxComponent(BaseComponent):
+
+    def __init__(self):
+        self.width = 30
+        self.height = 30
+
+
+class LabelComponent(BaseComponent):
+
+    def __init__(self):
+        self.width = 40
+        self.height = 40
+
+
+class ComponentsFactory(object):
+    @staticmethod
+    def create_component(comp_type):
+        if comp_type == 'Button':
+            return ButtonComponent()
+        elif comp_type == 'Canvas':
+            return CanvasComponent()
+        elif comp_type == 'TextBox':
+            return TextBoxComponent()
+        elif comp_type == 'Label':
+            return LabelComponent()
+
+
+if __name__ == '__main__':
+    for comp_type in ('Button', 'Canvas', 'TextBox', 'Label'):
+        print('Width is {0} of a {1}'.format(ComponentsFactory.create_component(comp_type).get_width(), comp_type))
